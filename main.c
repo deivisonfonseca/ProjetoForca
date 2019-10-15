a971dcc505a1bbd25e8b33fef7e7abef37a8602b
Eduardo <kayo.contaon@gmail.com>
2019-10-15 19:37:35 -0300

    Iniciado projeto

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+//#include "forca.c"
+#include "forca.c"
+
+void copyright(){
+    system("clear");
+    printf("=============================================================\n\n");
+    printf("JOGO DA FORCA\n");
+    printf("Desenvolvido por: \n\tDeivison - RA 18458\n");
+    printf("\tClaudio - RA 18456\n");
+    printf("=============================================================\n\n");
+}
+
+int limpaTela()
+{
+puts("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL,"");
+
+    NoSecreto * lstSecreta = inicializaListaSecreta();
+    char fNameArq[255];
+    NoSecreto * sorteada;
+    int posSorteada=0;
+    int tamanhoLista=0;
+
+    strcpy(fNameArq,"palavras.dat");
+
+    if(argc>1){
+      strcpy(fNameArq,argv[1]);
+    }
+
+    printf("Arquivo de dados: %s\n\n",fNameArq);
+
+    copyright();
+
+    lstSecreta = carregaListaArquivo(lstSecreta,fNameArq);
+    imprimeListaSecreta(lstSecreta);
+    tamanhoLista = tamanhoListaSecreta(lstSecreta);
+
+    do{
+        CLEAR_SCREEN;
+        imprimeListaSecreta(lstSecreta);
+
+        sorteada= sorteiaPalavra(lstSecreta);
+        if(sorteada!=NULL){
+            printf("%s\n", sorteada->palavra);
+        }else{
+            printf("Não há palavras disponíveis!\n\n");
+        }
+    }while(getchar()!='f');
+
+
+    return 0;
+}
